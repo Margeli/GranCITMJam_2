ffa95bdf3abf14d5984f3a28b91e79a4fc79884b
Margeli <niniopantalones@gmail.com>
2019-06-18 21:08:29 +0200

    added attack animation

---
+++ DeepCorrosion/Assets/ElectricStick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricStick : MonoBehaviour
+{
+    Animator anim;
+    Player playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerScript = transform.parent.GetComponent<Player>();
+
+    }
+
+    public void Attack()
+    {
+        anim.SetBool("attacking", true);
+        
+    }
+    private void EndAttack()
+    {
+        anim.SetBool("attacking", false);
+        playerScript.EndAttack();
+    }
+    private void doEffect()
+    {
+        Debug.Log("effect");
+
+    }
+}
