492007972f471bbdabe994a59d9585e15abf7af6
Margeli <niniopantalones@gmail.com>
2019-06-18 21:48:22 +0200

    added attack player

--- DeepCorrosion/Assets/Scripts/ElectricStick.cs
+++ DeepCorrosion/Assets/Scripts/ElectricStick.cs
@@ -6,24 +6,28 @@ public class ElectricStick : MonoBehaviour
 {
     Animator anim;
     Player playerScript;
+    StickCollider collider;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         playerScript = transform.parent.GetComponent<Player>();
+        collider = transform.GetChild(0).GetComponent<StickCollider>();
 
     }
 
     public void Attack()
     {
         anim.SetBool("attacking", true);
+        collider.gameObject.SetActive(true);
         
     }
     private void EndAttack()
     {
         anim.SetBool("attacking", false);
         playerScript.EndAttack();
+        collider.gameObject.SetActive(false);
     }
     private void doEffect()
     {
