91dbfeca39f69e80f65e3e5ab8f0999cbd04edbe
albertmas <almaco_io@hotmail.es>
2019-06-18 18:19:34 +0200

    Radar sprite

---
+++ DeepCorrosion/Assets/Radar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    GameObject player;
+    private float radar_frequency = 3.0f;
+    private float timer = 0.0f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= radar_frequency)
+        {
+            RadarSwipe();
+        }
+    }
+
+    void RadarSwipe()
+    {
+        Transform playertrans = player.transform;
+        //Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+    }
+}
