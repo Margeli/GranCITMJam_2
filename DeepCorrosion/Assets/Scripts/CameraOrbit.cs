ed26580df9c283b8590577fa3babcb17d971b04a
rodrigodpl <rodrigodpl97@gmail.com>
2019-06-19 12:36:56 +0200

    main menu

--- DeepCorrosion/Assets/Scripts/CameraOrbit.cs
+++ DeepCorrosion/Assets/Scripts/CameraOrbit.cs
@@ -8,6 +8,6 @@ public class CameraOrbit : MonoBehaviour
     void Update()
     {
         transform.RotateAround(Vector3.zero, Vector3.up, 0.02f);
-        transform.LookAt(Vector3.zero);
+        transform.LookAt(new Vector3(0.0f, 0.15f, 0.0f));
     }
 }
