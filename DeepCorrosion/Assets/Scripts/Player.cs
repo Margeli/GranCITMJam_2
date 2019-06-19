45ac7f729495e501a3e3a9673d9c2a8d98327071
Margeli <niniopantalones@gmail.com>
2019-06-19 13:18:03 +0200

    added

--- DeepCorrosion/Assets/Scripts/Player.cs
+++ DeepCorrosion/Assets/Scripts/Player.cs
@@ -38,6 +38,8 @@ public class Player : MonoBehaviour
     public bool grabbedBarrelBool = false;
     public bool attacking = false;
     private Rigidbody rb;
+    private bool regenerate = false;
+    public float regeneratePerSec = 10.0f;
 
 
 
@@ -65,6 +67,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (regenerate)
+        {
+            Debug.Log("Regenerate");
+            if (health < 100)
+            {
+                health += regeneratePerSec * Time.fixedDeltaTime;
+            }
+            if (health > 100)
+            {
+                health = 1000;
+            }
+        }
+
         rb.velocity = Vector3.zero;
         //-------------------------------------------------------------GRABBING
         if (!grabbedBarrelBool)
@@ -152,6 +167,7 @@ public class Player : MonoBehaviour
         if(grabbedBarrelBool && other.gameObject.tag == "DeliverSpot")
         {
             dropUI.SetActive(true);
+            regenerate = true;
             if (Input.GetKeyDown(KeyCode.E))//drop barrel in the subamrine
             {
                 electricStickScript.gameObject.SetActive(true);
@@ -171,6 +187,13 @@ public class Player : MonoBehaviour
         if (other.gameObject.tag == "Barrels")
         {
             grabUI.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Barrels")
+        {
+            regenerate = false;
         }
     }
 }
