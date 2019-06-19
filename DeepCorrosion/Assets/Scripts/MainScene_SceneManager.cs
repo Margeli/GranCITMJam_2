0661c7b82516d6a1097c9936aa61adb1446fff57
rodrigodpl <rodrigodpl97@gmail.com>
2019-06-19 13:17:14 +0200

    fade to black

--- DeepCorrosion/Assets/Scripts/MainScene_SceneManager.cs
+++ DeepCorrosion/Assets/Scripts/MainScene_SceneManager.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainScene_SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void ReturnToMenu()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
     }
 }
