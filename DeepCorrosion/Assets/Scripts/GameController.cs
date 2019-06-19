6910311a35e002e9304eb03822b0b262439aa314
rodrigodpl <rodrigodpl97@gmail.com>
2019-06-19 11:04:55 +0200

    bubbles fix

---
+++ DeepCorrosion/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.health < 0)
+        {
+            // game over
+        }
+        else if (player.totalBarrels == 0)
+        {
+            // you won!
+        }
+    }
+}
