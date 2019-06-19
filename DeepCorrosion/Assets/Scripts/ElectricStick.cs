e0005ff0258fd63224c772bbf6f1dfacb9127091
polferrando98 <pol.ferrando.98@gmail.com>
2019-06-19 05:35:44 +0200

    Added chispazo

--- DeepCorrosion/Assets/Scripts/ElectricStick.cs
+++ DeepCorrosion/Assets/Scripts/ElectricStick.cs
@@ -7,6 +7,7 @@ public class ElectricStick : MonoBehaviour
     Animator anim;
     Player playerScript;
     StickCollider collider;
+    ParticleSystem particle_system;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class ElectricStick : MonoBehaviour
         anim = GetComponent<Animator>();
         playerScript = transform.parent.GetComponent<Player>();
         collider = transform.GetChild(0).GetComponent<StickCollider>();
+        particle_system = transform.Find("Emitter Parent").GetComponent<ParticleSystem>();
 
     }
 
@@ -32,6 +34,28 @@ public class ElectricStick : MonoBehaviour
     private void doEffect()
     {
         Debug.Log("effect");
+        StartCoroutine(Countdown());
+
+    }
+
+    IEnumerator Countdown()
+    {
+
+        while (true)
+        {
+            if (!particle_system.gameObject.activeSelf)
+            {
+                particle_system.gameObject.SetActive(true);
+                yield return new WaitForSeconds(0.3f);
+            }
+            else
+            {
+                particle_system.gameObject.SetActive(false);
+                StopAllCoroutines();
+                yield return null;
+            }
+        }
+
 
     }
 }
