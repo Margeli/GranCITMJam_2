aef566111b671df3aa2d6b9104f286fa21773c13
rodrigodpl <rodrigodpl97@gmail.com>
2019-06-19 05:26:02 +0200

    prefab features

---
+++ DeepCorrosion/Assets/Scripts/FishSchool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSchool : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        direction = direction.normalized;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        direction = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        direction = direction.normalized;
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
